dfa0fa90e4642aa2afc0d13d06a7b940b0dac6aa
yuenchwork2020 <66813160+angus-ho@users.noreply.github.com>
2023-06-16 11:33:58 +0800

    added hook in SearchPramas

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ const App = () => {
       <h1>Adopt Me!</h1>
       <SearchParams />
 
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+
     </div>
   );
 };
--- src/SearchPramas.jsx
+++ src/SearchPramas.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 
-let counter = 0
 const SearchParams = () => {
     const [location, setLocation] = useState("");
+    const [animal, updateAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+    const breeds =[];
+
     return (
         <div className="search-params">
             <form>
@@ -16,6 +20,50 @@ const SearchParams = () => {
                         placeholder="Location" 
                     />
 
+                </label>
+
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={(e) => {
+                        updateAnimal(e.target.value);
+                        updateBreed("");
+                        }}
+                        onBlur={(e) => {
+                        updateAnimal(e.target.value);
+                        updateBreed("");
+                        }}
+                    >
+
+                        <option />
+                        {ANIMALS.map((animal) => (
+                        <option key={animal} value={animal}>
+                            {animal}
+                        </option>
+                        ))}
+                    </select>
+                </label>
+
+
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        disabled={!breeds.length}
+                        id="breed"
+                        value={breed}
+                        onChange={(e) => setBreed(e.target.value)}
+                        onBlur={(e) => setBreed(e.target.value)}
+                    >
+
+                        <option />
+                        {breeds.map((breed) => (
+                        <option key={breed} value={breed}>
+                            {breed}
+                        </option>
+                        ))}
+                    </select>
                 </label>
 
                 <button>Submit</button>
